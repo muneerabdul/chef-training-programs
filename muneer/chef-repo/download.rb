fd2d3e8216d74e61b453fd27e04f9914b6c79eb2
Muneer AJ <muneer4web@gmail.com>
2018-05-21 12:34:20 +0000

    More

--- muneer/chef-repo/download.rb
+++ muneer/chef-repo/download.rb
@@ -11,8 +11,9 @@ bash 'java' do
     mkdir -p /opt/Training/JavaTar
     cd /opt/Training/JavaTar
     wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
-    tar -xzf ./jdk-8u131-linux-x64.tar.gz -C /opt/Jdk
-    export JAVA_HOME=/opt/Jdk
+    mkdir -p /opt/Jdk
+    tar xzf ./jdk-8u131-linux-x64.tar.gz -C /opt/Jdk
+    export JAVA_HOME=/opt/Jdk/jdk1.8.0_131
     echo $JAVA_HOME > /opt/Training/echs.txt
     EOH
 end
