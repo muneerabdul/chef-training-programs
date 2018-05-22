14f572c11ae78b40239bb417eca65611ed3df55c
root <root@ip-172-31-90-165.ec2.internal>
2018-05-22 07:45:32 +0000

    Added by Vijay

--- vijayababu/chef-repo/cookbooks/vijay/recipes/default.rb
+++ vijayababu/chef-repo/cookbooks/vijay/recipes/default.rb
@@ -4,9 +4,6 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-
-
-file '/home/ec2-user/chef-training-programs/vijayababu/chef-repo/infosys.txt' do
-
-content 'welcome to infosys'
-end
+include_recipe 'vijay::file'
+include_recipe 'vijay::package'
+include_recipe 'vijay::service'
