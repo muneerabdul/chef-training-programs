9c629c68765f4630ff8552db7acef2129ccd7852
mpeddaia <mpeddaia@cisco.com>
2018-05-22 07:11:28 +0000

    added individual recipes for template,service,file

---
+++ mpeddaia/cookbooks/webserver/recipes/default1.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: webserver
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+#package 'httpd'
+
+file "/var/www/html/index.html" do
+ content "<h1> Welcome to Chef Class</h1></br><h2>From scmGalaxy</h2>"
+end 
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+
+service 'httpd' do
+  action :start
+end
