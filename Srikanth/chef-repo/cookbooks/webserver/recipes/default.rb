5a2d327817e06e9f4d887ee915ee1110c7d2d02e
root <root@ip-172-31-88-236.ec2.internal>
2018-05-22 09:15:58 +0000

    adding

--- Srikanth/chef-repo/cookbooks/webserver/recipes/default.rb
+++ Srikanth/chef-repo/cookbooks/webserver/recipes/default.rb
@@ -5,8 +5,11 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
 
-file "/var/www/html/index.html" do
- content "<h1> Welcome to Chef Class</h1></br><h2>From scmGalaxy</h2>"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
 end
 
 service 'httpd' do
