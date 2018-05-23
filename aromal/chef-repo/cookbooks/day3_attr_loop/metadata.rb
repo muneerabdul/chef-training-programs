4512362194bd67467ad70de112eb5666ee405638
root <root@devopsvm1.ow1qi4vclj1ujeb2jqbedafh5f.gx.internal.cloudapp.net>
2018-05-23 11:37:22 +0530

    added loops

---
+++ aromal/chef-repo/cookbooks/day3_attr_loop/metadata.rb
@@ -0,0 +1,20 @@
+name 'day3_attr_loop'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures day3_attr_loop'
+long_description 'Installs/Configures day3_attr_loop'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/day3_attr_loop/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/day3_attr_loop'
