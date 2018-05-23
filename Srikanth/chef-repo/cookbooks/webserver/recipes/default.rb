f915e5aa709fc01931c04f81e093f2d09c42ea38
root <root@ip-172-31-88-236.ec2.internal>
2018-05-23 07:32:45 +0000

    chef-solo added

--- Srikanth/chef-repo/cookbooks/webserver/recipes/default.rb
+++ Srikanth/chef-repo/cookbooks/webserver/recipes/default.rb
@@ -1,1 +1,17 @@
-default['webserver']['name']= 'Srikanth Jagdhane'
+#package 'httpd'
+
+#file "/var/www/html/index.html" do
+# content "<h1> Welcome to Chef Class</h1></br><h2>From Srikanth</h2>"
+#end
+
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#end
+
+#service 'httpd' do
+# action :start
+#end
+
--- Srikanth/chef-repo/cookbooks/webserver/recipes/file.rb
+++ Srikanth/chef-repo/cookbooks/webserver/recipes/file.rb
@@ -1,12 +1,8 @@
-myname=node['webserver']['name']
 
 file "/opt/infosys.txt" do
  content "Hi.. my name is #{node['webserver']['name']} "
 end
 
 
-file "/opt/name.txt" do
- content "Hi.. my name is #{myname}"
-end
 
 
