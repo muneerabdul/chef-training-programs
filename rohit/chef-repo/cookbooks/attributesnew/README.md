d3469993a31bb84a29750d579332b27972b1bfe3
root <root@ip-172-31-29-196.ap-south-1.compute.internal>
2018-05-23 07:42:04 +0000

    adding chef solo

---
+++ rohit/chef-repo/cookbooks/attributesnew/README.md
@@ -0,0 +1,4 @@
+# attributesnew
+
+TODO: Enter the cookbook description here.
+
---
+++ rohit/chef-repo/solo.rb
@@ -0,0 +1,5 @@
+file_cache_path "/root/chef-solo/cache"
+json_attribs "/root/chef-solo/json"
+cookbook_path "/root/chef-training-programs/rohit/chef-repo/cookbooks"
+log_level :debug
+
