8fd7338d4d103384c66eb796724f7b5d749f487d
Anju Philip <anjuphilip2209@gmail.com>
2018-05-23 06:30:39 +0000

    Adding day3-labs

--- anju/chef-repo/cookbooks/webserver/recipes/default.rb
+++ anju/chef-repo/cookbooks/webserver/recipes/default.rb
@@ -4,16 +4,20 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-include_recipe 'webserver::file'
+#include_recipe 'webserver::file'
 
-include_recipe 'webserver::file'
+#include_recipe 'webserver::file'
 
-package 'httpd'
+#package 'httpd'
 
-file "/var/www/html/index.html" do
- content "<h1> Welcome to Chef Class</h1></br><h2>From scmGalaxy</h2>"
-end
+#file "/var/www/html/index.html" do
+ #content "<h1> Welcome to Chef Class</h1></br><h2>From scmGalaxy</h2>"
+#end
 
-service 'httpd' do
-  action :start
-end
+#service 'httpd' do
+#  action :start
+#end
+
+include_recipe 'webserver::day3_loop1'
+
+include_recipe 'webserver::day3_loop2'
