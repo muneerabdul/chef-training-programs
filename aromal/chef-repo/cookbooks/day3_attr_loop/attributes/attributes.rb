d0ce59d5bc1393eecd223404c8bc596ad571eacf
root <root@devopsvm1.ow1qi4vclj1ujeb2jqbedafh5f.gx.internal.cloudapp.net>
2018-05-23 12:02:33 +0530

    splitting variable

--- aromal/chef-repo/cookbooks/day3_attr_loop/attributes/attributes.rb
+++ aromal/chef-repo/cookbooks/day3_attr_loop/attributes/attributes.rb
@@ -1,6 +1,3 @@
 default['day3_attr_loop']['shared_dir'] = 'shared-lib'
 default['day3_attr_loop']['config_dir'] = 'shared-config'
-default['day3_attr_loop']['mode'] = 755
-default['day3_attr_loop']['owner'] = "engsp"
-default['day3_attr_loop']['group'] = "root"
 
