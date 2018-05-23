511a68cab3fc0c967954e3e0b19a51ff49b6201e
root <root@ip-172-31-17-233.ap-south-1.compute.internal>
2018-05-23 06:29:38 +0000

    adding files

---
+++ uma/chef-repo/cookbooks/Chef-attribute/attribute/attribute.rb
@@ -0,0 +1,5 @@
+default['attribute']['shared_dir'] = 'shared-lib'
+default['attribute']['config_dir'] = 'shared-config'
+default['attribute']['mode'] = 755
+default['attribute']['owner'] = "ec2-user"
+default['attribute']['group'] = "root"
